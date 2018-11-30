df7c97668a455c44d15c58d08a4a3ae99e2ccc66
crystal07 <crystal090807@gmail.com>
2018-12-01 06:34:36 +0900

    before css

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,206 @@
 <template>
-  <div id="app">
-    <img src="./icons/bee.png" class="bee-image">
-    <h1>{{ msg }}</h1>
+  <div class="root-layout">
+    <div class="timezone-selection">
+      <select onfocus="this.size=6;" onchange="this.size=1; this.blur();" v-model="timezone">
+        <option value selected disabled hidden>Timezone</option>
+        <option v-for="(zone, index) in timezone_list" :key="index">{{zone}}</option>
+      </select>
+    </div>
+    <div class="time-show-layout">
+      <div class="time-wrapper">
+        <div class="date">{{ date }}</div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="time">{{ time }}</div>
+      </div>
+    </div>
+    <div class="list-show-layout">
+      <button v-on:click="change_show_state(0)">memo</button>
+      <span class="list-show-bar">|</span>
+      <button v-on:click="change_show_state(1)">todo</button>
+    </div>
+    <div v-if="todoShow" class="list-layout" style="right: calc(3% + 121px);"></div>
+    <div v-if="memoShow" class="list-layout">
+      <memo></memo>
+    </div>
   </div>
 </template>
 
 <script>
-import config from './config';
+var moment = require("moment-timezone");
+import memo from "./memo/MemoLayout.vue";
 export default {
-  name: 'app',
-  data () {
+  name: "time",
+  data() {
     return {
-      msg: 'Welcome to your chrome extension',
-      image_base: config.IMAGE_BASE,
+      time: "",
+      date: "",
+      timezone: "Asia/Seoul",
+      timezone_list: null,
+      todoShow: false,
+      memoShow: false
+    };
+  },
+  components: {
+    memo
+  },
+  beforeCreate: function() {},
+  mounted: function() {
+    this.show_time();
+    moment.tz.setDefault("Asia/Seoul");
+    this.timezone_list = moment.tz.names();
+  },
+  watch: {
+    timezone: function() {
+      this.show_time();
     }
   },
-}
+  methods: {
+    show_time: function() {
+      var timerID = setInterval(this.update_time, 1000);
+      this.update_time();
+    },
+    update_time: function() {
+      this.date = moment()
+        .tz(this.timezone)
+        .format("YYYY[.]MM[.]DD[.]");
+      this.time = moment()
+        .tz(this.timezone)
+        .format("HH:mm:ssa");
+    },
+    change_show_state: function(obj) {
+      console.log("show state", obj);
+      if (obj == 0) {
+        this.memoShow = !this.memoShow;
+        this.todoShow = false;
+      } else if (obj == 1) {
+        this.memoShow = false;
+        this.todoShow = !this.todoShow;
+      } else {
+        this.memoShow = false;
+        this.todoShow = false;
+      }
+    }
+  }
+};
 </script>
 
-<style lang="scss">
-body {
-  width: 300px;
-  height: 300px;
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.root-layout {
+  display: block;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-color: transparent;
 }
-#app {
-  color: #2c3e50;
-  margin-top: 60px;
+
+.timezone-selection {
+  display: block;
+  position: relative;
+  width: 100%;
+  height: 10%;
 }
-.bee-image {
-  width: 128px;
+
+.timezone-selection select {
+  outline: 0px;
+  font-size: 100%;
+  position: absolute;
+  float: right;
+  width: max-content;
+  text-align-last: right;
+  border: 0px;
+  outline: 0px;
+  background-color: transparent;
+  overflow: scroll;
+  font-size: 25px;
+  top: 25%;
+  z-index: 1;
+  right: 1%;
+}
+
+.time-show-layout {
+  position: relative;
+  height: 80%;
+}
+
+.time-show-layout .time-wrapper {
+  position: relative;
+  display: inline-block;
+  top: 30%;
+}
+
+.time-show-layout .date {
+  /* font-size: 150px; */
+  display: block;
+  position: relative;
+  float: left;
+  font-size: 56px;
+  /* font-weight: bold; */
+}
+
+.time-show-layout .time {
+  /* font-size: 200px; */
+  display: block;
+  position: relative;
+  float: left;
+  font-size: 86px;
+  font-weight: bold;
+  top: -15px;
+}
+
+.list-show-layout {
+  position: relative;
+  height: 10%;
+  font-size: 100%;
+  padding-right: 3%;
+}
+
+.list-show-layout button {
+  border: 0px;
+  outline: 0px;
+  float: right;
+  font-size: 30px;
+  font-weight: bold;
+  z-index: 2;
+}
+
+.list-show-bar {
+  float: right;
+  font-size: 35px;
+}
+
+.list-layout {
+  /* background-image: linear-gradient(
+    to right,
+    red,
+    orange,
+    yellow,
+    violet,
+    yellow,
+    orange,
+    red
+  ); */
+  background-color: #ccc;
+  position: absolute;
+  width: 280px;
+  height: 200px;
+  bottom: calc(55px + 5%);
+  right: calc(3% + 23px);
+  border-radius: 15px;
+  z-index: 3;
+}
+
+.list-layout:after {
+  content: "";
+  position: absolute;
+  border-top: 30px solid #ccc;
+  border-right: 15px solid transparent;
+  border-left: 15px solid transparent;
+  bottom: -26px;
+  right: 15px;
+  z-index: 1;
 }
 </style>
-
