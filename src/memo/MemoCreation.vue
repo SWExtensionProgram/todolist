57cfec660b040b7311fc801d1f9137c9c13f550e
crystal07 <crystal090807@gmail.com>
2018-12-04 20:38:46 +0900

    basic struction in memo creation, detail, moidification view

---
+++ src/memo/MemoCreation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="root-layout"></div>
+</template>
+
+<script>
+export default {
+  name: "MemoModification",
+  mounted: function() {},
+  props: ["memo"],
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.root-layout {
+  display: block;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-color: transparent;
+}
+</style>
